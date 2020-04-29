8361bab2d03ba3d42e09fbc426ee842cdcb6eeae
Justin <47774897+Mr-silentcoder@users.noreply.github.com>
2020-04-29 17:46:32 +0530

    Delete referral.php

--- referral.php
+++
@@ -1,9 +0,0 @@
-<?php
-    if(isset($_GET['code'])) {
-    	//add your site url 
-    	header("Location: https://yourappname.herokuapp.com");
-    	ext();
-        ;}
-               else {
-                echo '<b>referral code error...!<b>';
-                       }?>
